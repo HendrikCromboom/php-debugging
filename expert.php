3c1de071a38bdafc9447e2511fb0d9bd6e97aa8c
HENDRIK <hendrik@KALI.home>
2020-10-02 12:10:07 +0200

    done

--- expert.php
+++ expert.php
@@ -1,28 +1,21 @@
 <?php
 declare(strict_types=1);
-//1
-new_exercise();
-function new_exercise() {
-    $x = 1;
+echo "Exercise 1 starts here:";
+function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise ".$x." starts here:<br/>";
     echo $block;
 }
-echo "<br>";
-//2
+new_exercise(2);
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 $monday = $week[0];
 
 echo $monday;
 
-echo "<br><br>";
-//3
-
+new_exercise(3);
 $str = "Debugged ! Also very fun";
 echo substr($str, 0, 10);
 
-echo "<br><br>";
-
-//4
+new_exercise(4);
 
 
 foreach($week as &$day) {
@@ -31,14 +24,111 @@ foreach($week as &$day) {
 
 print_r($week);
 
-echo "<br><br>";
-//5
+new_exercise(5);
 
 $arr = [];
-for ($letter = 'a'; $letter <= 'z'; $letter++) {
+for ($letter = "a"; $letter <= "z" && $letter != "aa"; $letter++) {
     array_push($arr, $letter);
 }
 
 print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
 
+//$array = range('a', 'z'); is best solution
+
+new_exercise(6);
+
+$arr = [];
+
+
+function combineNames($str1 = "", $str2 = "") {
+    $params = [$str1, $str2];
+    foreach($params as &$param) {
+        if ($param == "") {
+            $param = randomHeroName();
+        }
+    }
+    return implode( " - ", $params);
+}
+
+
+function randomGenerate($arr, $amount) {
+    for ($i = $amount; $i > 0; $i--) {
+        array_push($arr, randomHeroName());
+    }
+
+    return $arr;
+}
+
+function randomHeroName()
+{
+    $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
+    $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
+    $heroes = [$hero_firstnames, $hero_lastnames];
+    $randname = $heroes[rand(0,count($heroes))][rand(0, 10)];
+
+    return $randname;
+}
+
+echo "Here is the name: " . combineNames();
+
+new_exercise(7);
+
+
+function copyright(int $year) {
+    echo "&copy; $year BeCode";
+}
+copyright(intval(date('Y')));
+
+new_exercise(8);
+function login(string $email, string $password) {
+    if($email == 'john@example.be' && $password == 'dfgidfgdfg') {
+        return 'Welcome John Smith';
+
+    }
+    return 'No access';
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+    $error = 0;
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) !== false) {
+            echo 'Unacceptable Found<br />';
+            $error ++;
+        }
+
+    }if($error==0){echo 'Acceptable<br />';}
+
+}
+//invalid link
+isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+isLinkValid('https://google.com');
+//VALID link
+isLinkValid('http://google.com');
+//VALID link
+isLinkValid('http://google.com/test.txt');
+
+
+new_exercise("10");
+
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$counter = count($areTheseFruits);
+for($i=0; $i < $counter; $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
 
